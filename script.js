52d82067b1d82e7c2c52528f7e31255c64cd5b71
Dineth Fernando <156776845+DinethSheveen@users.noreply.github.com>
2025-08-11 21:09:36 +0100

    Reassured to have all the conditions to be true to generate the passsword

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//DOM SELECTION
 const passwordLength = document.querySelector(".user-input");
 const errorMsg = document.querySelector(".error-msg")
 const includeLowercase = document.querySelector(".lowercase")
@@ -27,11 +28,13 @@ function generatePassword(){
         errorMsg.innerHTML = "Password generated successfully"
     }
 
+    //SELECTING THE ALLOWED CHARACTERS FOR THE PASSWORD
     allowedChars += includeLowercase.checked?lowerCase:""
     allowedChars += includeUppercase.checked?upperCase:"";
     allowedChars += includeNumbers.checked?numbers:"";
     allowedChars += includeSymbols.checked?symbols:"";
 
+    //DISPLAYING AN ERROR MSG IF NEITHER OF THE RADIO BUTTONS ARE SELECTED
     if(allowedChars === ""){
         errorMsg.classList.remove("no-error")
         errorMsg.classList.add("error")
@@ -39,7 +42,7 @@ function generatePassword(){
     }
 
     //GENRERATING THE PASSWORD IF THE CONDITIONS ARE SATISFIED
-    else{
+    else if(allowedChars !== "" && passwordLength.value>=5){
         for(let i=0;i<passwordLength.value;i++){
             let randomNumber = Math.floor(Math.random()*allowedChars.length);
             password += allowedChars[randomNumber] 
